5292f99b82ac473c1230ef9e3e585c48dde52976
Jan T. Sott <jan@idleberg.com>
2015-10-06 22:35:32 +0200

    update linters

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,42 @@
 var gulp = require('gulp');
 var debug = require('gulp-debug');
-var jsonlint = require('gulp-json-lint');
 var coffeelint = require('gulp-coffeelint');
+var jshint = require('gulp-jshint');
+var jsonlint = require('gulp-jsonlint');
 
 // Tasks
-gulp.task('lint', ['coffeelint', 'jsonlint']);
+gulp.task('lint', ['coffeelint', 'jshint', 'jsonlint', 'recess']);
 
 // Exclude node_modules
 var self = '!node_modules/**/*';
 
-// Lint CSON files
+// Lint CoffeeScript & CSON files
 gulp.task('coffeelint', function () {
-    gulp.src([
-        './**/*.cson', './**/*.coffee', self])
-        .pipe(debug({title: 'csonlint:'}))
+    gulp.src(['./**/*.cson', './**/*.coffee', self])
+        .pipe(debug({title: 'coffeelint:'}))
         .pipe(coffeelint())
         .pipe(coffeelint.reporter())
+});
+
+// Lint LESS files
+gulp.task('recess', function () {
+     gulp.src(['./**/*.less', self])
+        .pipe(debug({title: 'recess:'}))
+        .pipe(recess({"noOverqualifying": false} ))
+        .pipe(recess.reporter());
+});
+
+// Lint JavaScript files
+gulp.task('jshint', function() {
+    return gulp.src(['./**/*.js', self])
+        .pipe(debug({title: 'jshint:'}))
+        .pipe(jshint())
 });
 
 // Lint JSON files
 gulp.task('jsonlint', function() {
    gulp.src(['./**/*.json', self])
-      .pipe(debug({title: 'jsonlint:'}))
-      .pipe(jsonlint())
-      .pipe(jsonlint.report('verbose'));
+        .pipe(debug({title: 'jsonlint:'}))
+        .pipe(jsonlint())
+        .pipe(jsonlint.reporter());
 });
