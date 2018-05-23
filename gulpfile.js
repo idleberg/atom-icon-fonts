02764b9b127a5bb21dd8360eeec1ed3b0028bf70
idleberg <jan@idleberg.com>
2018-05-23 20:29:07 +0200

    remove unused tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,7 @@
 const coffeelint = require('gulp-coffeelint');
 const debug = require('gulp-debug');
 const gulp = require('gulp');
-const jshint = require('gulp-jshint');
 const jsonlint = require('gulp-jsonlint');
-const lesshint = require('gulp-lesshint');
 const stylish = require('coffeelint-stylish');
 
 // Files
@@ -16,10 +14,6 @@ const coffeeFiles = [
     './snippets/*.cson'
 ];
 
-const jsFiles = [
-    './lib/**/*.js'
-];
-
 const jsonFiles = [
     './grammars/*.json',
     './keymaps/*.json',
@@ -28,48 +22,27 @@ const jsonFiles = [
     './package.json'
 ];
 
-const lessFiles = [
-    './styles/**/*.less'
-];
-
 // Lint CoffeeScript & CSON files
-gulp.task('lint:coffee', gulp.series(function(done) { 
+gulp.task('lint:coffee', (done) => {
     gulp.src(coffeeFiles)
         .pipe(debug({title: 'coffeelint:'}))
         .pipe(coffeelint())
         .pipe(coffeelint.reporter(stylish))
         .pipe(coffeelint.reporter('fail'));
     done();
-}));
-
-// Lint LESS files
-gulp.task('lint:less', gulp.series(function(done) { 
-    gulp.src(lessFiles)
-        .pipe(debug({title: 'lesshint:'}))
-        .pipe(lesshint())
-        .pipe(lesshint.reporter());
-    done();
-}));
-
-// Lint JavaScript files
-gulp.task('lint:js', gulp.series(function(done) { 
-    gulp.src(jsFiles)
-        .pipe(debug({title: 'jshint:'}))
-        .pipe(jshint());
-    done();
-}));
+});
 
 // Lint JSON files
-gulp.task('lint:json', gulp.series(function(done) { 
+gulp.task('lint:json', (done) => {
     gulp.src(jsonFiles)
         .pipe(debug({title: 'jsonlint:'}))
         .pipe(jsonlint())
         .pipe(jsonlint.failAfterError())
         .pipe(jsonlint.reporter());
     done();
-}));
+});
 
 // Tasks
-gulp.task('lint', gulp.parallel('lint:coffee', 'lint:js', 'lint:json', 'lint:less', function(done) {
+gulp.task('lint', gulp.parallel('lint:coffee', 'lint:json', done => {
   done();
 }));
